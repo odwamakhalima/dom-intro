351c2fd233f8f70a3e0d1ca9eecf94499d227402
Odwa Makhalima <3652687@myuwc.ac.za>
2019-05-17 10:18:20 +0200

    DOM

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -3,55 +3,82 @@ var callTotalSettings = document.querySelector('.callTotalSettings')
 var smsTotalSettings = document.querySelector('.smsTotalSettings')
 var totalSettings = document.querySelector('.totalSettings')
 var settingsBillAddBtn = document.querySelector('.settingsBillAddBtn')
+var totalCost = 0;
 var callsTotal3 = 0;
 var smsTotal3 = 0;
 var callCost2 = 0;
 var smsCost2 = 0;
+var warningLevel = 0;
+var criticalLevel = 0;
 var criticalLevelSetting = document.querySelector('.criticalLevelSetting')
 var warningLevelSetting = document.querySelector('.warningLevelSetting')
 var smsCostSetting = document.querySelector('.smsCostSetting')
 var callCostSetting = document.querySelector('.callCostSetting')
 var updateSettings = document.querySelector('.updateSettings')
-function update(){
-    callCost2 = Number(callCostSetting.value)
-    smsCost2 = Number(smsCostSetting.value)
-    warningLevel = Number(warningLevelSetting.value)
-    criticalLevel = Number(criticalLevelSetting.value)
+function update() {
+    callCost2 = Number(callCostSetting.value);
+    smsCost2 = Number(smsCostSetting.value);
+    warningLevel = Number(warningLevelSetting.value);
+    criticalLevel = Number(criticalLevelSetting.value);
+
+    if (totalCost >= warningLevel) {
+        totalSettings.classList.add("warning");
+        totalSettings.classList.remove("danger");
     }
-    updateSettings.addEventListener('click', update);
-function settingsBill(){
+    if (totalCost < warningLevel) {
+        totalSettings.classList.remove("danger");
+        totalSettings.classList.remove("warning");
+    }
+
+    if (totalCost >= criticalLevel) {
+        totalSettings.classList.add("danger");
+        totalSettings.classList.remove("warning");
+
+    }
+    if (totalCost >= criticalLevel) {
+        settingsBillAddBtn.removeEventListener("click", settingsBill);
+
+    }
+    else {
+        settingsBillAddBtn.addEventListener("click", settingsBill);
+    }
+}
+
+function settingsBill() {
     var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-    if (checkedRadioBtn){
+    if (checkedRadioBtn) {
         var billItemType = checkedRadioBtn.value
     }
-    if (billItemType === "call"){
+    if (billItemType === "call") {
         callsTotal3 += callCost2;
     }
-    else if (billItemType === "sms"){
+    else if (billItemType === "sms") {
         smsTotal3 += smsCost2;
     }
     callTotalSettings.innerHTML = callsTotal3.toFixed(2);
     smsTotalSettings.innerHTML = smsTotal3.toFixed(2);
-    var totalCost = callsTotal3 + smsTotal3;
+    totalCost = callsTotal3 + smsTotal3;
     totalSettings.innerHTML = totalCost.toFixed(2);
 
-    if(totalCost >= warningLevel){
+    if (totalCost >= warningLevel) {
         totalSettings.classList.add("warning");
         totalSettings.classList.remove("danger");
     }
-    if(totalCost < warningLevel){
-        totalSettings.classList.remove("danger")
-        totalSettings.classList.remove("warning")
+    if (totalCost < warningLevel) {
+        totalSettings.classList.remove("danger");
+        totalSettings.classList.remove("warning");
     }
- 
-    do{
-        if(totalCost >= criticalLevel){
-            settingsBillAddBtn.removeEventListener("click",settingsBill );
-            totalSettings.classList.add("danger")
-            totalSettings.classList.remove("warning")
-        }
-        
-    }while(totalCost > criticalLevel);
+
+    if (totalCost >= criticalLevel) {
+
+        totalSettings.classList.add("danger");
+        totalSettings.classList.remove("warning");
+    }
+    if (totalCost >= criticalLevel) {
+        settingsBillAddBtn.removeEventListener("click", settingsBill);
+    }
 
 }
 settingsBillAddBtn.addEventListener('click', settingsBill);
+updateSettings.addEventListener('click', update);
+updateSettings.addEventListener('click', update);
